49a6ef7cf5ccacf3ac06d567ed10afe3eef4aaae
tankor-cpu <tanakornienko08@gmail.com>
2025-05-24 19:23:11 +0200

    Initial commit – Spring Boot backend project

---
+++ is/src/main/java/cz/vsb/is/InformacniSystemApplication.java
@@ -0,0 +1,13 @@
+package cz.vsb.is;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformacniSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformacniSystemApplication.class, args);
+	}
+
+}
